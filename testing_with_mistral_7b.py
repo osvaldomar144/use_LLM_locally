4b0043bf790e051baa31fbede6e3f01e431f8de4
Alexis <osvaldomar144@gmail.com>
2024-04-03 10:25:29 +0200

    fix: testing with mistral 7B and 8 bit

--- testing_with_mistral_7b.py
+++ testing_with_mistral_7b.py
@@ -1,21 +1,32 @@
-from transformers import AutoModelForSequenceClassification
-import torch
+from huggingface_hub import hf_hub_download
+from llama_cpp import Llama
 
-# Caricare il modello
-model = AutoModelForSequenceClassification.from_pretrained("jncraton/Mistral-7B-Instruct-v0.2-ct2-int8")
+# define model name and file
+model_name = "TheBloke/Mistral-7B-Instruct-v0.2-GGUF"
+model_file = "mistral-7b-instruct-v0.2.Q8_0.gguf"
 
-# Preprocessare i dati
-tokenizer = model.config.tokenizer
-input_ids = tokenizer.encode("Ciao mondo!")
+model_path = hf_hub_download(model_name, filename=model_file)
 
-# Eseguire il modello
-outputs = model(input_ids)
+model_kwargs = {
+  "n_ctx":4096,    # Context length to use
+  "n_threads":4,   # Number of CPU threads to use
+  "n_gpu_layers":0,# Number of model layers to offload to GPU. value = 0 beacuse use only CPU
+}
 
-# Ottenere i risultati
-logits = outputs.logits
+## Instantiate model from downloaded file
+llm = Llama(model_path=model_path, **model_kwargs)
 
-# Use PyTorch's softmax function
-probabilities = torch.nn.functional.softmax(logits, dim=1)
+## Generation kwargs
+generation_kwargs = {
+    "max_tokens":200, # Max number of new tokens to generate
+    "stop":["<|endoftext|>", "</s>"], # Text sequences to stop generation on
+    "echo":False, # Echo the prompt in the output
+    "top_k":1 # This is essentially greedy decoding, since the model will always return the highest-probability token. Set this value > 1 for sampling decoding
+}
 
-# Stampare i risultati
-print(probabilities)
+## Run inference
+prompt = "The meaning of life is "
+res = llm(prompt, **generation_kwargs) # Res is a dictionary
+
+## Unpack and the generated text from the LLM response dictionary and print it
+print(res["choices"][0]["text"])
