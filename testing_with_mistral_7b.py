8227879395ede216c9a7fac3bc1f3ff20f2fbe28
Alexis Martinez <cons.alexis.martinez@cy4gate.com>
2024-04-03 16:09:45 +0200

    chore: refactoring code

--- testing_with_mistral_7b.py
+++ testing_with_mistral_7b.py
@@ -8,25 +8,27 @@ model_file = "mistral-7b-instruct-v0.2.Q8_0.gguf"
 model_path = hf_hub_download(model_name, filename=model_file)
 
 model_kwargs = {
-  "n_ctx":4096,    # Context length to use
-  "n_threads":4,   # Number of CPU threads to use
-  "n_gpu_layers":0,# Number of model layers to offload to GPU. value = 0 beacuse use only CPU
+  "n_ctx": 4096,    # Context length to use
+  "n_threads": 4,   # Number of CPU threads to use
+  "n_gpu_layers": 0,  # Number of model layers to offload to GPU. value = 0 beacuse use only CPU
 }
 
-## Instantiate model from downloaded file
+# Instantiate model from downloaded file
 llm = Llama(model_path=model_path, **model_kwargs)
 
-## Generation kwargs
+# Generation kwargs
 generation_kwargs = {
-    "max_tokens":200, # Max number of new tokens to generate
-    "stop":["<|endoftext|>", "</s>"], # Text sequences to stop generation on
-    "echo":False, # Echo the prompt in the output
-    "top_k":1 # This is essentially greedy decoding, since the model will always return the highest-probability token. Set this value > 1 for sampling decoding
+    "max_tokens": 200,  # Max number of new tokens to generate
+    "stop": ["<|endoftext|>", "</s>"],  # Text sequences to stop generation on
+    "echo": False,  # Echo the prompt in the output
+    "top_k": 1  # This is essentially greedy decoding, since the model will always return the highest-probability token.
+                # Value > 1 for sampling decoding
 }
 
-## Run inference
-prompt = "The meaning of life is "
-res = llm(prompt, **generation_kwargs) # Res is a dictionary
+# Run inference
+prompt = "Write a text about conflict in Ukraine"
+res = llm(prompt, **generation_kwargs)
 
-## Unpack and the generated text from the LLM response dictionary and print it
+# Unpack and the generated text from the LLM response dictionary and print it
+print("\n\n-------- RESPONSE MODEL ---------\n")
 print(res["choices"][0]["text"])
